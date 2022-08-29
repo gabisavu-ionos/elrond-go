4b7fc26e09cd94ba0a73463c45bf9a714715327e
Iulian Pascalau <iulian.pascalau@gmail.com>
2022-08-29 18:43:57 +0300

    - probable fix + more prints

--- node/nodeDebugFactory/interceptedDebugHandler.go
+++ node/nodeDebugFactory/interceptedDebugHandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ElrondNetwork/elrond-go-core/core/check"
+	logger "github.com/ElrondNetwork/elrond-go-logger"
 	"github.com/ElrondNetwork/elrond-go/config"
 	"github.com/ElrondNetwork/elrond-go/dataRetriever"
 	"github.com/ElrondNetwork/elrond-go/debug/factory"
@@ -12,6 +13,8 @@ import (
 
 // InterceptorResolverDebugger is the contant string for the debugger
 const InterceptorResolverDebugger = "interceptor resolver debugger"
+
+var log = logger.GetOrCreate("node")
 
 // CreateInterceptedDebugHandler creates and applies an interceptor-resolver debug handler
 func CreateInterceptedDebugHandler(
@@ -30,11 +33,15 @@ func CreateInterceptedDebugHandler(
 		return ErrNilResolverContainer
 	}
 
+	log.Debug("REMOVE_ME start factory.NewInterceptorResolverDebuggerFactory")
 	debugHandler, err := factory.NewInterceptorResolverDebuggerFactory(config)
 	if err != nil {
+		log.Debug("REMOVE_ME finish with error factory.NewInterceptorResolverDebuggerFactory", "error", err)
 		return err
 	}
+	log.Debug("REMOVE_ME finish factory.NewInterceptorResolverDebuggerFactory")
 
+	log.Debug("REMOVE_ME start iterating on interceptors")
 	var errFound error
 	interceptors.Iterate(func(key string, interceptor process.Interceptor) bool {
 		err = interceptor.SetInterceptedDebugHandler(debugHandler)
@@ -45,10 +52,12 @@ func CreateInterceptedDebugHandler(
 
 		return true
 	})
+	log.Debug("REMOVE_ME finished iterating on interceptors", "error found", errFound)
 	if errFound != nil {
 		return fmt.Errorf("%w while setting up debugger on interceptors", errFound)
 	}
 
+	log.Debug("REMOVE_ME start iterating on resolvers")
 	resolvers.Iterate(func(key string, resolver dataRetriever.Resolver) bool {
 		err = resolver.SetResolverDebugHandler(debugHandler)
 		if err != nil {
@@ -58,9 +67,14 @@ func CreateInterceptedDebugHandler(
 
 		return true
 	})
+	log.Debug("REMOVE_ME finished iterating on resolvers", "error found", errFound)
 	if errFound != nil {
 		return fmt.Errorf("%w while setting up debugger on resolvers", errFound)
 	}
 
-	return node.AddQueryHandler(InterceptorResolverDebugger, debugHandler)
+	log.Debug("REMOVE_ME adding query handler")
+	err = node.AddQueryHandler(InterceptorResolverDebugger, debugHandler)
+	log.Debug("REMOVE_ME added query handler", "error", err)
+
+	return err
 }
